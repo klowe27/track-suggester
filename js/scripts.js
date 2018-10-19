d28f3164c5692c9dca86ef599438de193e61f52b
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-10-19 10:00:07 -0700

    Add basic branching to tailor results

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,18 @@ $(document).ready(function(){
     var preference = $("input:radio[name=preference]:checked").val();
     var environment = $("input:radio[name=environment]:checked").val();
     var adjective = $("input:radio[name=adjective]:checked").val();
+
+    $(".result").hide();
+
+    if (environment === "large" || preference === "backend") {
+      $(".c").show();
+      $(".name").empty().append(name);
+    } else if (preference === "flexible") {
+      $(".php").show();
+      $(".name").empty().append(name);
+    } else {
+      $(".ruby").show();
+      $(".name").empty().append(name);
+    }
   });
 });
